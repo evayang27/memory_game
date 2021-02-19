98e5a699e6b462b27e9c18c0a27be61146ef3a73
Eva <k1094t8@gmail.com>
2021-02-19 22:30:49 +0800

    add flip cards function

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
 // 符號圖檔
 const Symbols = [
   'https://image.flaticon.com/icons/svg/105/105223.svg', // 黑桃
-  'https://www.flaticon.com/svg/vstatic/svg/2107/2107845.svg?token=exp=1613719251~hmac=981f1dbea6296477c4ebbde4ee221b8c', // 愛心
-  'https://www.flaticon.com/svg/vstatic/svg/138/138534.svg?token=exp=1613719482~hmac=b6c127536298dd2c95cfb82623ca494c', // 方塊
+  'https://www.flaticon.com/svg/vstatic/svg/2107/2107845.svg?token=exp=1613744394~hmac=57d0bcbe13b3a4dcf62f88e40a3bf8d3', // 愛心
+  'https://www.flaticon.com/svg/vstatic/svg/138/138534.svg?token=exp=1613744416~hmac=4ba34829fc9d00f7d0705644856cb04c', // 方塊
   'https://image.flaticon.com/icons/svg/105/105219.svg' // 梅花
 ]
 
 // view layer
 const view = {
   // 顯示單張卡片正面 -> 拆成兩部分 innerHTML 內容另設 function
-  // 1-1. 單一卡片正面
+  // 1-1. 增加卡片DOM(背面)
   getCardElement(index) {
+    return `
+      <div class="card-box">
+        <div class="card back" data-index="${index}"></div>
+      </div>
+    `
+  },
+  // 1-2. 取得卡片內容
+  getCardContent(index) {
     // 卡片數字花色用 0-51 去求 
     const cardNumber = this.transformNumber((index % 13) + 1)
     const cardSymbol = Symbols[Math.floor(index / 13)]
     return `
-      <div class="card-box">
-        <div class="card" data-index="${index}">
-          <p>${cardNumber}</p>
-          <img src="${cardSymbol}" alt="">
-          <p>${cardNumber}</p>
-        </div>
-      </div>
+        <p>${cardNumber}</p>
+        <img src="${cardSymbol}" alt="">
+        <p>${cardNumber}</p>
     `
   },
-  // 1-2. A J Q K 數字變換
+  // 1-3. A J Q K 數字變換
   transformNumber(number) {
     switch (number) {
       case 1:
@@ -39,7 +43,7 @@ const view = {
         return number
     }
   },
-  // 1-3. heart diamond cards -> red
+  // 1-4. heart diamond cards -> red
   transformColor() {
     const cardList = document.querySelectorAll('.card')
     cardList.forEach(card => {
@@ -53,13 +57,48 @@ const view = {
   displayCards() {
     const cardPanel = document.querySelector('#cards')
     // 產生 0-51 arr -> 呼叫 getCardElement 52次 -> 合成內容放進#cards
-    cardPanel.innerHTML = Array.from(Array(52).keys()).map(index => this.getCardElement(index)).join('')
+    // array先洗牌
+    cardPanel.innerHTML = utility.getRandomNumberArray(52).map(index => this.getCardElement(index)).join('')
     this.transformColor()
   },
+  // 3. 翻牌
+  flipCard(card) {
+    console.log('flips')
+    if (card.classList.contains('back')) {
+      card.classList.remove('back')
+      card.innerHTML = this.getCardContent(Number(card.dataset.index))
+      return
+    }
+    card.classList.add('back')
+    card.innerHTML = null
+  }
+}
 
+// utility 已知工具
+const utility = {
+  // 洗牌 Fisher-Yates Shuffle(Knuth-Shuffle) algor, 最後一項跟前面的換 
+  // count 放 arr length 這邊是指一組牌數量52
+  getRandomNumberArray(count) {
+    const numberArray = Array.from(Array(count).keys())
+    // 長度52 最後一項是arr[51] 所以-1
+    for (let theLastIndex = numberArray.length - 1; theLastIndex > 0; theLastIndex--) {
+      // random [0,1) 目標是0-51 Math.floor([0,1*52)) 所以+1
+      let randomIndex = Math.floor(Math.random() * (theLastIndex + 1))
+        // ;不能省略
+        ;[numberArray[theLastIndex], numberArray[randomIndex]] = [numberArray[randomIndex], numberArray[theLastIndex]]
+    }
+    return numberArray
+  },
 }
 
 
 // 初始render
 view.displayCards()
+
+// event flip card 每張牌都加監聽器 要在初始render後面
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('click', event => {
+    view.flipCard(card)
+  })
+})
 
